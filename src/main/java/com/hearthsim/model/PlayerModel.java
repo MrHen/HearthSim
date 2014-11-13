46a4a2cc6307d535731442c95c4fa56d9da5768f
Adam Babcock <github@mrhen.com>
2014-11-13 13:16:04 -0600

    Pulled node creation into handler routines in order to prep for more testable node generation. (Basically, this means tests can generate one round of nodes without needing to do the full recursive build.)

--- src/main/java/com/hearthsim/model/PlayerModel.java
+++ src/main/java/com/hearthsim/model/PlayerModel.java
@@ -41,6 +41,10 @@ public class PlayerModel implements DeepCopyable<PlayerModel> {
         return card;
     }
 
+    public int getNumCharacters() {
+    	return this.getNumMinions() + 1;
+    }
+    
     public int getNumMinions() {
         return minions.size();
     }
